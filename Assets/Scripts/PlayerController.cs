6043e1b1ceefb38cf6fbe9bf15af2677e00625a4
VolodymyrHusliev <vova@antari.io>
2023-09-30 20:45:20 +0200

    -add- player mouse controll

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,16 +9,52 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Vector3 direction;
     [SerializeField] private Rigidbody rb;
-    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float jumpForce;
+    [SerializeField] float moveSpeedMouse;
     [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isCursorVisible;
 
      public void Start()
     {
         rb = GetComponent<Rigidbody>();
+        LockCursor();
     }
 
+     private void LockCursor()
+     {
+         Cursor.lockState = CursorLockMode.Locked;
+         Cursor.visible = false;
+         isCursorVisible = false;
+     }
+
+     private void UnlockCursor()
+     {
+         Cursor.lockState = CursorLockMode.None;
+         Cursor.visible = true;
+         isCursorVisible = true;
+     }
+    
     public void Update()
     {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UnlockCursor();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            LockCursor();
+        }
+
+        if (!isCursorVisible)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            Vector3 moveDirection = new Vector3(mouseX, 0f, 0f);
+            moveDirection.Normalize();
+            transform.Translate(moveDirection * (moveSpeedMouse * Time.deltaTime));
+        }
+        
+        
         transform.Translate(direction);
         
         if (isGrounded && Input.GetMouseButtonDown(0))
